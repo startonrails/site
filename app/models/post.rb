900587f9fb0c92fb58ff476a0ab90cec3cdfcd4f
Vivi Sousa <vivocah@gmail.com>
2012-05-23 21:40:47 -0300

    criação coluna published

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,3 @@
 class Post < ActiveRecord::Base
-  attr_accessible :description, :title
+  attr_accessible :description, :title, :published
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120523230301) do
+ActiveRecord::Schema.define(:version => 20120524001448) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.boolean  "published"
   end
 
 end
