e1a4d753867fd91fe3d7ab97a078c37d84ce620c
vivocah <vivocah@gmail.com>
2012-09-20 21:55:17 -0300

    criação do index comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,11 @@
 class CommentsController < ApplicationController
   respond_to :html, :json
+  layout 'admin'
+
+    def index
+  	  @comments = Comment.all
+
+ 	end
 
   def create
     @comment = Comment.new(params[:comment])
@@ -8,5 +14,5 @@ class CommentsController < ApplicationController
 
       redirect_to root_path
     end
-  end
+  end		
 end
